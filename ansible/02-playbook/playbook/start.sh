23f717c1a05593ae70b877a3be8ef43b7612fc09
Kovrei <rei169kov@yandex.ru>
2025-03-10 22:48:04 +0500

    error

--- ansible/02-playbook/playbook/start.sh
+++ ansible/02-playbook/playbook/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker start clickhouse-01
-docker start vector
+docker start vector-01
 
 
 #ansible-playbook ~/devops-netology/ansible/02-playbbok/playbook/site.yml -i ~/devops-netology/ansible/02-playbook/playbook/inventory/prod.yml
